297ee37231bac7e0a0bab179b15d8aa3b0bc08dc
Luizenrique1 <48364368+Luizenrique1@users.noreply.github.com>
2019-06-07 07:11:49 -0500

    Add files via upload

---
+++ Sesion12_ActividadPaquetes/src/edu/tecjerez/topicos/dosdimensiones/Circulo.java
@@ -0,0 +1,14 @@
+package edu.tecjerez.topicos.dosdimensiones;
+
+public class Circulo {
+
+	public double areaCirculo(double radio) {
+		
+		return( (3.1416)*(radio*radio));
+	}
+	
+	public double perimetroCirculo(double radio ) {
+		return (2*3.1416*radio);
+	}
+}
+
---
+++ Sesion12_ActividadPaquetes/src/edu/tecjerez/topicos/dosdimensiones/Rombo.java
@@ -0,0 +1,12 @@
+package edu.tecjerez.topicos.dosdimensiones;
+
+public class Rombo {
+
+	static double perimetroRombo(double lado) {
+		return 4*lado;
+	}
+	
+	public double areaRombo(double d,double D) {
+		return (d*D)/2;
+	}
+}
